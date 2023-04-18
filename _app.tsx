141ea53545b4e8b4a00f32ce0bea8b40e7ea4239
Tivrida <75439110+Tivrida@users.noreply.github.com>
2023-04-18 23:18:28 +0300

    PRT_6
    PRT_6

---
+++ _app.tsx
@@ -0,0 +1,14 @@
+import '@/styles/globals.css'
+import { CssBaseline, PaletteColorOptions, ThemeProvider } from '@mui/material'
+import type { AppProps } from 'next/app'
+import { createTheme } from "@mui/material/styles";
+
+export default function App({ Component, pageProps }: AppProps) {
+    return <Component {...pageProps} />
+    return <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+    </ThemeProvider>
+}
+
+const theme = createTheme();
